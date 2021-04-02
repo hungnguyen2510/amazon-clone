aeb6f0db0df23f022e0fca3b7b110f0a3f2d13ac
hungnguyen2510 <htmm2510@gmail.com>
2021-04-02 08:22:51 +0700

    login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -11,6 +11,21 @@ const Login = () => {
           src="https://www.offers.vn/wp-content/uploads/2016/06/mua-hang-amazon.png"
         ></img>
       </Link>
+      <div className="login_container">
+        <h1>Sign In</h1>
+        <form>
+          <h5>Email</h5>
+          <input type="email" />
+          <h5>Password</h5>
+          <input type="password" />
+          <button className="login_signInButton">Sign In</button>
+          <p>
+            By continuing, you agree to Amazon's Conditions of Use and Privacy
+            Notice.
+          </p>
+          <button className="login_registerButton">Create your amazon account</button>
+        </form>
+      </div>
     </div>
   );
 };
